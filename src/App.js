9d6d9b6fcd981466d497cd3115ece89344d837ad
Muryllo <murylloazvd@gmail.com>
2021-09-13 19:54:43 -0300

    logo and container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { DatePicker } from 'antd';
+import { ReactComponent as Icon } from './assets/building.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="logo">
+        <Icon className="logo__icon" />
+        SETORES
+      </div>
+      <main>
+        <div className="sectors"></div>
+        <div className="addSectors"></div>
+      </main>
     </div>
   );
 }
