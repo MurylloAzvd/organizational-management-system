3255f231fce623e0ddee77a43534345dfaae8fef
Muryllo <murylloazvd@gmail.com>
2021-09-14 01:45:28 -0300

    similar layout

--- src/components/AddSector/index.js
+++ src/components/AddSector/index.js
@@ -1,7 +1,36 @@
 import './style.css'
+import { CloseCircleFilled } from '@ant-design/icons'
 
 function AddSector() {
-    return (<div className="addSector"></div>)
+    return (
+        <div className="addSector">
+            <h1>ADICIONAR SETOR</h1>
+
+            <h2>NOME:</h2>
+            <div className="inputContainer">
+                <input type="text" />
+            </div>
+
+            <h2>CARGO(S):</h2>
+            <div className="inputContainer">
+                <input type="text" />
+                <button>ADICIONAR</button>
+            </div>
+
+            <div className="tags">
+                <div className="tagContainer">
+                    <span className="tagTitle">CARGO 1</span>
+                    <CloseCircleFilled className="tagIcon" />
+                </div>
+                <div className="tagContainer">
+                    <span className="tagTitle">CARGO 24747478585858</span>
+                    <CloseCircleFilled className="tagIcon" />
+                </div>
+            </div>
+
+            <button className="saveButton">SALVAR</button>
+        </div>
+    )
 }
 
 export default AddSector;
